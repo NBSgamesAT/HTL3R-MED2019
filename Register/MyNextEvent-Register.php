2509a4323df1a27124bf5c71ef3e99b514922eb6
Shunpomeister <49274790+Shunpomeister@users.noreply.github.com>
2019-05-10 21:51:08 +0200

    Update MyNextEvent-Register.php

--- Register/MyNextEvent-Register.php
+++ Register/MyNextEvent-Register.php
@@ -31,7 +31,7 @@
                 $execute->bindParam(':geschlecht', $geschlecht);
                 $execute->execute();
 	    }
-		header("Location: ../MyNextEvent-Login/MyNextEvent-Login.php");
+		header("Location: ../Login/MyNextEvent-Login.php");
 	    ?>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
