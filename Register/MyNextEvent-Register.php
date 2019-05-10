c7aa20e8bc8bb81f7a41a84f6d509f00c11cc727
Shunpomeister <49274790+Shunpomeister@users.noreply.github.com>
2019-05-10 21:50:28 +0200

    Update MyNextEvent-Register.php

--- Register/MyNextEvent-Register.php
+++ Register/MyNextEvent-Register.php
@@ -13,7 +13,7 @@
 
             <?php
             
-            if(isset($_POST["benutzername"]) && isset($_POST["password"]) && isset($_POST["geburtsdatum"]) && isset($_POST["gender"]) && isset($_POST["ort"]) ){
+            if(isset($_POST["benutzername"]) && isset($_POST["password"]) && isset($_POST["bday"]) && isset($_POST["gender"]) && isset($_POST["ort"]) ){
                 
                 $user = $_POST["benutzername"];
                 $pw = $_POST["password"];
@@ -30,35 +30,9 @@
                 $execute->bindParam(':geb', $geb);
                 $execute->bindParam(':geschlecht', $geschlecht);
                 $execute->execute();
-
-				if (null == $user ) {
-					echo "Benutzername: Pflichtfeld nicht eingegeben!";
-				} else {
-					echo "Benutzername: " .$user;
-				}
-				echo "</br>";
-				if (null == $pw ) {
-					echo "Passwort: Pflichtfeld nicht eingegeben!";
-				} else {
-					echo "Passwort: " .$pw;
-				}
-				echo "</br>";
-					if ($geschlecht == 'Mann') {
-						echo "Geschlecht: ".$geschlecht;
-					}
-					else {
-						echo "Geschlecht: ".$geschlecht;
-					}
-				echo "</br>";
-				echo "Ort: ".$ort;
-				echo "</br>";
-				if (is_numeric($_POST['Tag']) == true && is_numeric($_POST['Monat']) == true && is_numeric($_POST['Jahr']) == true)	{
-						echo "Geburtsdatum: ".$_POST['Tag']. "-" .$_POST['Monat']. "-" .$_POST['Jahr'];
-				} else {
-					echo "Datum: Pflichtfeld nicht eingegeben oder falscher Wert!";
-                }
-            }
-			?>
+	    }
+		header("Location: ../MyNextEvent-Login/MyNextEvent-Login.php");
+	    ?>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
